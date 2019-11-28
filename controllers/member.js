a73579e02b9ca1f78d6948227dd9290e61870379
Daniel Yoo <onikss793@gmail.com>
2019-11-28 18:13:42 +0900

    Delete endpoint
    1. Find member to be deleted
    2. If you can't find a member, throw an error
    3. If you found one delete him/her
    4. Give response about a memebr you just deleted

--- controllers/member.js
+++ controllers/member.js
@@ -1,42 +1,65 @@
-const memberServices = require("services/member");
 const Member = require("models/member");
 const Cell = require("models/cell");
 
+const {
+  getMemberList,
+  addNewMember,
+  updateMember,
+  deleteMember
+} = require("services/member");
+
 exports.memberList = async (req, res) => {
   try {
-    const members = await memberServices.getMemberList();
+    const members = await getMemberList();
 
     res.status(200).json(members);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ error: err.message });
   }
 };
 
 exports.addMember = async (req, res) => {
   try {
-    const member = await memberServices.addNewMember(req);
+    const member = await addNewMember(req);
 
     res.status(200).json({
       message: "Success",
-      member
+      created: member
     });
   } catch (err) {
     const member = err.keyValue.nickName;
 
-    res.status(400).json({ message: `member '${member}' already exists` });
+    res.status(400).json({ error: `member '${member}' already exists` });
   }
 };
 
 exports.updateMemberDetails = async (req, res) => {
   try {
-    const member = await memberServices.updateMember(req);
+    const member = await updateMember(req);
 
     !member
-      ? res.status(400).json({ message: "Not a Member" })
+      ? res.status(400).json({ error: "Not a Member" })
       : res.status(200).json({
           message: "success",
-          member
+          updated: member
         });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.deleteMember = async (req, res) => {
+  try {
+    const memberId = req.params.memberId;
+
+    const deleted = await deleteMember(memberId);
+
+    deleted
+      ? res.status(200).json({
+          message: "success",
+          deleted
+        })
+      : res.status(400).json({ error: "Member does not exist" });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
--- routes/member.js
+++ routes/member.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const memberControllers = require("controllers/member");
+const {
+  memberList,
+  addMember,
+  updateMemberDetails,
+  deleteMember
+} = require("controllers/member");
 
-router.get("/", memberControllers.memberList);
-router.post("/", memberControllers.addMember);
-router.post("/:memberId", memberControllers.updateMemberDetails);
+router.get("/", memberList);
+router.post("/", addMember);
+router.post("/:memberId", updateMemberDetails);
+router.delete("/:memberId", deleteMember);
 
 module.exports = router;
